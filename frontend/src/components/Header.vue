99ca687186d57979bb788ad3c6b013bc77e161bc
michlampert <michlampert@gmail.com>
2021-05-31 01:53:36 +0200

    style homepage

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,30 +1,32 @@
 <template>
-    <nav class="navbar is-light is-fixed-top">
-        <div class="navbar-brand">
-            <router-link to="/" class="navbar-item">
-                <figure class="image is-64x64 mx-4">
-                    <img src="../assets/logo.png" style="max-height: 128px; max-width: 128px;">
-                </figure>
-                <h1 class="title mx-4">
-                    BAIron
-                </h1>
-            </router-link>
-            <a class="navbar-burger" id="burger">
-                <span></span>
-                <span></span>
-                <span></span>
-            </a>
-        </div>
-        <div class="navbar-menu" id="nav-links">
-            <div class="navbar-end">
-                <router-link class="navbar-item" to="/">Home</router-link>
-                <router-link class="navbar-item" to="/about">About</router-link>
-                <router-link class="navbar-item" to="/generate">Generator</router-link>
-                <router-link class="navbar-item" to="/poems">Poems</router-link>
+    <div>
+        <nav class="navbar is-light is-fixed-top">
+            <div class="navbar-brand">
+                <router-link to="/" class="navbar-item">
+                    <figure class="image is-64x64 mx-4">
+                        <img src="../assets/logo.png" style="max-height: 128px; max-width: 128px;">
+                    </figure>
+                    <h1 class="title mx-4">
+                        BAIron
+                    </h1>
+                </router-link>
+                <a class="navbar-burger" id="burger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </a>
             </div>
-        </div>
+            <div class="navbar-menu" id="nav-links">
+                <div class="navbar-end">
+                    <router-link class="navbar-item" to="/">Home</router-link>
+                    <router-link class="navbar-item" to="/about">About</router-link>
+                    <router-link class="navbar-item" to="/generate">Generator</router-link>
+                    <router-link class="navbar-item" to="/poems">Poems</router-link>
+                </div>
+            </div>
 
-    </nav>
+        </nav>
+    </div>
 </template>
 
 <script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -9,15 +9,27 @@
     </main> -->
     <div class="container">
         <div class="columns is-vcentered has-text-centered">
-            <div class="column is-7-desktop">
-                <h1 class="is-size-1-tablet is-size-2-mobile has-text-weight-bold mb-6">
-                    Become world class artist and create your own masterpiece!
-                </h1>
-                <router-link to="/generate">
-                    <button class="button is-primary is-large is-rounded">
-                        Check it out!
-                    </button>
-                </router-link>
+            <div class="column is-7-desktop is-12-tablet">
+                <div class="hero is-fullheight-with-navbar">
+                    <div class="hero-body">
+                        <div>
+                            <h1 class="is-size-1-tablet is-size-2-mobile has-text-weight-bold">
+                                Become world class artist and create your own masterpiece!
+                            </h1>
+                            <router-link to="/generate">
+                                <button class="button is-primary is-large is-rounded my-5">
+                                    Check it out!
+                                </button>
+                            </router-link>
+                            <h2 class="is-size-4-tablet is-size-5-mobile">
+                                ... or just check others popular poems <router-link to="/poems" class="has-text-primary has-text-weight-bold">here</router-link>
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-5-desktop is-hidden-touch">
+                <Gallery />
             </div>
         </div>
     </div>
