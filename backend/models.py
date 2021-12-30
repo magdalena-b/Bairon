a9cedda365fbd6df80d26b7c8ee66fe9ed4f3c9f
magdalena_b <magdexxa@gmail.com>
2021-12-30 19:39:47 +0100

    Add model_type field to Poem model

--- backend/models.py
+++ backend/models.py
@@ -28,6 +28,7 @@ class Poem(models.Model):
     views = models.IntegerField(default=0)
     sentiment = models.CharField(max_length=100, null=True, blank=True)
     generator_type = models.CharField(max_length=100, default="full")
+    model_type = models.CharField(max_length=100, default="gpt2")
     style_transfer = models.IntegerField(default=0)
     translations = models.CharField(max_length=100000, default="", blank=True)
     bleu_score = models.IntegerField(null=True, blank=True)
